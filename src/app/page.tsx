e316cc954a8172552d5f01972da6aa83a44661b2
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2025-03-26 16:40:13 +0530

    fix : dark mode bg color for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,18 @@ import { useTheme } from 'next-themes'
 
 export default function Home() {
   const { setTheme, theme } = useTheme()
+  const isDarkTheme = theme === 'dark'
   return (
     <div className='flex min-h-screen flex-col items-center justify-items-center w-full font-[family-name:var(--font-inter-tight)]'>
       <div style={{ backgroundSize: '22.05px auto', backgroundPosition: 'top left', opacity: '0.1' }} className='absolute h-full top-0 right-0 left-0 -z-1 bg-repeat bg-[url(https://framerusercontent.com/images/zkZcqLYKrbf3IcoLGmkQF4odXvY.svg)]'></div>
       <div className='absolute w-full top-0 right-0 left-0 z-10 border-b h-16'></div>
       <main className='flex justify-center h-screen w-full'>
         <div className='lg:w-[948px] border-x w-full flex flex-col justify-start items-center z-10'>
-          <div className='h-16 w-full flex items-center justify-between text-sm text-center sm:text-left py-4 px-2 border-b font-semibold'>
+          <div className='h-16 w-full flex items-center justify-between text-sm text-center sm:text-left py-4 px-2 border-b font-semibold bg-white dark:bg-[#232323]'>
             <div>⚡ZEUS</div>
-            {theme === 'light' ? <MoonStar onClick={() => setTheme('dark')} size={18} className='cursor-pointer mr-2' /> : <Sun onClick={() => setTheme('light')} size={18} className='cursor-pointer mr-2' />}
+            {!isDarkTheme ? <MoonStar onClick={() => setTheme('dark')} size={18} className='cursor-pointer mr-2' /> : <Sun onClick={() => setTheme('light')} size={18} className='cursor-pointer mr-2' />}
           </div>
-          <div className='w-full flex flex-col gap-3 justify-center items-center' style={{ backgroundSize: '14px 14px', backgroundImage: `linear-gradient(to right,#f6f6f6 1px,transparent 1px),linear-gradient(to bottom,#f6f6f6 1px,transparent 1px)` }}>
+          <div className='w-full flex flex-col gap-3 justify-center items-center' style={{ backgroundSize: '14px 14px', backgroundImage: `linear-gradient(to right,${isDarkTheme ? '#272727' : '#f6f6f6'} 1px,transparent 1px),linear-gradient(to bottom,${isDarkTheme ? '#272727' : '#f6f6f6'} 1px,transparent 1px)` }}>
             <h2 className='text-6xl font-semibold mt-10'>Ultimate Workout Logging</h2>
             <div className='px-2 mb-2 text-sm text-center font-[family-name:var(--font-geist-mono)]'>A powerful fitness companion designed to track, analyze, and optimize your training progress. Simple, intuitive, and built to help you achieve your fitness goals.</div>
             <Button className='mb-4 cursor-pointer'>Start Now 🔥</Button>
