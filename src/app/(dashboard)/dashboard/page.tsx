52f97a5bc58065c644f50fc8e5babe76a02383a6
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2025-04-11 15:27:53 +0530

    feat : settings pop up and home page started

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,9 +2,19 @@
 import React from 'react'
 
 const Page = () => {
+  const date = new Date().toDateString().split(' ')
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center'>
-      <div className='px-8'></div>
+    <div className='h-full w-full py-3 px-4'>
+      <div className='flex justify-between items-center'>
+        <div>
+          <div className='text-sm text-neutral-500 dark:text-neutral-400 font-[family-name:var(--font-geist-mono)]'>Welcome,</div>
+          <div className='text-sm opacity-90'>Anmol Chaudhary</div>
+        </div>
+        <div className='flex flex-col items-end'>
+          <div className='text-sm text-neutral-500 dark:text-neutral-400 font-[family-name:var(--font-geist-mono)]'>🌤️Today</div>
+          <div className='text-sm opacity-90 ml-1'>{`${date[2]} ${date[1]}, ${date[3]}`}</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,17 +1,34 @@
 'use client'
 import { useState } from 'react'
 
-import { Settings as SettingsIcon } from 'lucide-react'
+import { Settings as SettingsIcon, Palette, TimerReset, UserCog, Weight } from 'lucide-react'
 import { ResponsiveDialog } from '@/components/others/responsive-dialog'
+
+import { startCase } from 'lodash'
 
 import ThemeToggle from '@/components/others/theme-toggle'
 const Settings = () => {
   const [open, setOpen] = useState(false)
+  const items = ['appearance', 'restTimer', 'units', 'account']
+  const icons = {
+    [items[0]]: <Palette size={24} className='cursor-pointer mb-2 opacity-75' />,
+    [items[1]]: <TimerReset size={24} className='cursor-pointer mb-2 opacity-75' />,
+    [items[2]]: <Weight size={24} className='cursor-pointer mb-2 opacity-75' />,
+    [items[3]]: <UserCog size={24} className='cursor-pointer mb-2 opacity-75' />,
+  }
   return (
     <div>
       <SettingsIcon size={18} className='cursor-pointer mr-2' onClick={() => setOpen(true)} />
       <ResponsiveDialog title='Settings' description='Update your preferences' open={open} onOpenChange={setOpen}>
-        <ThemeToggle />
+        <div className='grid grid-cols-2 md:grid-cols-4 mt-3 gap-2'>
+          {items?.map(item => (
+            <div key={item} className='flex items-center flex-col justify-center bg-secondary border rounded-sm py-2 aspect-square cursor-pointer hover:bg-background/50'>
+              {icons[item]}
+              <span className='text-sm opacity-80'>{startCase(item)}</span>
+            </div>
+          ))}
+        </div>
+        {/* <ThemeToggle /> */}
       </ResponsiveDialog>
     </div>
   )
