dc0120dad5ebb54f4a729a14f9f5765076e85637
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2025-04-13 17:53:55 +0530

    feat : logout added

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 import React from 'react'
 
+import { authClient } from '@/lib/auth-client'
+
 const Page = () => {
   const date = new Date().toDateString().split(' ')
+  const { data: session } = authClient.useSession()
   return (
     <div className='h-full w-full py-3 px-4'>
       <div className='flex justify-between items-center'>
         <div>
           <div className='text-sm text-neutral-500 dark:text-neutral-400 font-[family-name:var(--font-geist-mono)]'>Welcome,</div>
-          <div className='text-sm opacity-90'>Anmol Chaudhary</div>
+          <div className='text-sm opacity-90'>{session?.user?.name}</div>
         </div>
         <div className='flex flex-col items-end'>
           <div className='text-sm text-neutral-500 dark:text-neutral-400 font-[family-name:var(--font-geist-mono)]'>🌤️Today</div>
--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -6,10 +6,13 @@ import { ResponsiveDialog } from '@/components/others/responsive-dialog'
 
 import { startCase } from 'lodash'
 
+import { authClient } from '@/lib/auth-client'
+import { useRouter } from 'next/navigation'
 // import ThemeToggle from '@/components/others/theme-toggle'
 const Settings = () => {
   const [open, setOpen] = useState(false)
   const items = ['appearance', 'restTimer', 'units', 'account', 'logOut', 'credit']
+  const router = useRouter()
   const icons = {
     [items[0]]: <Palette size={24} className='cursor-pointer mb-2 opacity-75' />,
     [items[1]]: <TimerReset size={24} className='cursor-pointer mb-2 opacity-75' />,
@@ -17,13 +20,20 @@ const Settings = () => {
     [items[3]]: <UserCog size={24} className='cursor-pointer mb-2 opacity-75' />,
     [items[4]]: <LogOut size={24} className='cursor-pointer mb-2 opacity-75' />,
   }
+  const logOut = async () => await authClient.signOut({ fetchOptions: { onSuccess: () => router.push('/') } })
+  const openGithub = () => window.open('https://github.com/4nmolChaudhary', '_')
+  const handleClick = (type: string) => {
+    if (type === 'logOut') return logOut()
+    if (type === 'credit') return openGithub()
+    console.log(type)
+  }
   return (
     <div>
       <SettingsIcon size={18} className='cursor-pointer mr-2' onClick={() => setOpen(true)} />
       <ResponsiveDialog title='Settings' description='Update your preferences' open={open} onOpenChange={setOpen}>
         <div className='grid grid-cols-2 md:grid-cols-4 mt-3 gap-2'>
           {items?.map((item, index) => (
-            <div key={item} className={`flex items-center flex-col justify-center rounded-sm py-2 cursor-pointer ${index === 5 ? 'md:col-span-3 col-span-2' : 'md:aspect-square'} ${index === 4 ? `md:col-span-1 col-span-2 bg-rose-900 border border-rose-600 hover:bg-rose-600/50` : `bg-secondary border hover:bg-background/50`}`}>
+            <div onClick={() => handleClick(item)} key={item} className={`flex items-center flex-col justify-center rounded-sm py-2 cursor-pointer ${index === 5 ? 'md:col-span-3 col-span-2' : 'md:aspect-square'} ${index === 4 ? `md:col-span-1 col-span-2 bg-rose-900 border border-rose-600 hover:bg-rose-600/50` : `bg-secondary border hover:bg-background/50`}`}>
               {index !== 5 ? (
                 <>
                   {icons[item]}
